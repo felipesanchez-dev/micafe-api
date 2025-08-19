ec37549fe4fd38e4586ba6464669b61351d04d70
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-08-18 20:36:39 -0500

    fix(api): eliminar comentario innecesario y ajustar la inclusión de archivos en tsconfig

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,5 @@ import { App } from "../src/app";
 const app = new App();
 const expressApp = app.getApp();
 
-// Export the Express app directly for Vercel
 module.exports = expressApp;
 export default expressApp;
